61c50049df4bfa0626497924c7324f00b673bcc8
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-09-08 16:31:52 -0300

    organização das pasta

---
+++ frontend/vite-project/node_modules/source-map-js/source-map.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2009-2011 Mozilla Foundation and contributors
+ * Licensed under the New BSD license. See LICENSE.txt or:
+ * http://opensource.org/licenses/BSD-3-Clause
+ */
+exports.SourceMapGenerator = require('source-map-js/lib/source-map-generator').SourceMapGenerator;
+exports.SourceMapConsumer = require('source-map-js/lib/source-map-consumer').SourceMapConsumer;
+exports.SourceNode = require('source-map-js/lib/source-node').SourceNode;
