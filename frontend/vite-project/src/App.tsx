deb58e7325f602084d29ccf612a6278a6fb732c0
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-10-03 11:00:35 -0300

    Página de Login feita

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
+import Login from './components/pages/Login'
 
 function App(){
     return (
         <div>
-            <Home/>
-            <About/>
-            <Contact/>
+            <Login/>
         </div>
     );
 }
---
+++ frontend/vite-project/src/components/pages/Login.tsx
@@ -0,0 +1,125 @@
+import React, {useState} from 'react';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import image from './images/usina_icon.png';
+import {
+    MDBContainer,
+    MDBBtn,
+    MDBIcon,
+    MDBInput,
+    MDBRow,
+    MDBCol,
+    MDBNavbar,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBNavbarToggler,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownMenu,
+    MDBDropdownToggle,
+    MDBDropdownItem, MDBNavbarBrand,
+}
+    from 'mdb-react-ui-kit';
+
+function Login() {
+    const [showNavCentred, setShowNavCentred] = useState(false);
+    return (
+        <MDBContainer className="my-5 gradient-form">
+
+            <MDBRow>
+                <MDBNavbar expand='lg' light bgColor='light'>
+                    <MDBContainer fluid>
+                        <MDBNavbarToggler
+                            type='button'
+                            data-target='#navbarCenteredExample'
+                            aria-controls='navbarCenteredExample'
+                            aria-expanded='false'
+                            aria-label='Toggle navigation'
+                            onClick={() => setShowNavCentred(!showNavCentred)}
+                        >
+                            <MDBIcon icon='bars' fas />
+                        </MDBNavbarToggler>
+                        <MDBNavbarBrand href='#'>
+                            <img
+                                src={image}
+                                width={100}
+                                alt=''
+                            />
+                        </MDBNavbarBrand>
+
+                        <MDBCollapse navbar show={showNavCentred} className="justify-content-center my-4 mb-4" id='navbarCenteredExample'>
+                            <MDBNavbarNav fullWidth={false}  className='mb-2 mb-lg-0'>
+
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink active aria-current='page' href='#'>
+                                        Página Inicial
+                                    </MDBNavbarLink>
+                                </MDBNavbarItem>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#'>Doação</MDBNavbarLink>
+                                </MDBNavbarItem>
+
+                                <MDBNavbarItem>
+                                    <MDBDropdown>
+                                        <MDBDropdownToggle tag='a' className='nav-link'>
+                                            Sobre nós
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu>
+                                            <MDBDropdownItem link>Quem nós somos</MDBDropdownItem>
+                                            <MDBDropdownItem link>Nossa História</MDBDropdownItem>
+                                            <MDBDropdownItem link>Nossos Eventos</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                </MDBNavbarItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBContainer>
+                </MDBNavbar>
+
+                <MDBCol col='6' className="mb-5">
+                    <div className="d-flex flex-column ms-5">
+                        <div className="text-center mb-7 my-5 ">
+                            <p>Sign in with:</p>
+
+                            <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
+                                <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                                    <MDBIcon fab icon='facebook-f' size="lg"/>
+                                </MDBBtn>
+
+                                <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                                    <MDBIcon fab icon='google' size="lg"/>
+                                </MDBBtn>
+                            </div>
+                        </div>
+
+                        <p>Faça login na sua conta</p>
+
+
+                        <MDBInput wrapperClass='mb-4' label='Endereço de E-mail' id='form1' type='email'/>
+                        <MDBInput wrapperClass='mb-4' label='Senha' id='form2' type='password'/>
+
+
+                        <div className="text-center pt-1 mb-5 pb-1">
+                            <MDBBtn className="mb-4 w-100 gradient-custom-2">Faça login</MDBBtn>
+                            <a className="text-muted" href="#!">Esqueceu sua senha?</a>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
+                            <p className="mb-0">Não tem uma conta ainda?</p>
+                            <MDBBtn outline className='mx-2' color='danger'>
+                                Cadastre-se
+                            </MDBBtn>
+                        </div>
+
+                    </div>
+
+                </MDBCol>
+
+            </MDBRow>
+
+        </MDBContainer>
+    );
+}
+
+export default Login;
