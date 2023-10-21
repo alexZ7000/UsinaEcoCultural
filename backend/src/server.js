a1e74bee2916018af2e823f8ee7b5027264f90ff
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-10-21 16:22:37 -0300

    delete e put terminados

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('You are connected with the database!');
 });
 
 app.use("/api/v1/users", userRoutes);
--- backend/src/users/routes.js
+++ backend/src/users/routes.js
@@ -5,6 +5,9 @@ const router = Router();
 router.get('/', controller.getUsers);
 router.get('/:id', controller.getUsersById); //qualquer valor que eu colocar depois da barra do meu users vai ser o id (sem os dois pontinhos)
 
-router.post('/', controller.createUser);
+router.post('/', controller.addUser);
+router.delete("/:id", controller.deleteUser);
+
+router.put("/:id", controller.updateUser);
 
 module.exports = router;
